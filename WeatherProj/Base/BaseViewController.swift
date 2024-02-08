e9b3ddf26ac2b38d6423ea15affd41f62910bd3c
chaewon-moana <dreamytaurus508@gmail.com>
2024-02-08 22:23:19 +0900

    BaseView 설정

---
+++ WeatherProj/Base/BaseViewController.swift
@@ -0,0 +1,34 @@
+//
+//  BaseViewController.swift
+//  WeatherProj
+//
+//  Created by cho on 2/8/24.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setAddView()
+        configureAttribute()
+        configureLayout()
+
+    }
+    
+
+    func setAddView() {
+        
+    }
+    
+    func configureAttribute() {
+        
+    }
+    
+    func configureLayout() {
+        
+    }
+
+}
